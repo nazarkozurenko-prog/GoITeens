a040a636643876f12bf7eb52d7b1cd002694caca
nazarkozurenko-prog <nazarkozurenko@gmail.com>
2026-07-02 19:39:01 +0300

    2

--- Assets/Scripts/AnimHandler.cs
+++ Assets/Scripts/AnimHandler.cs
@@ -1,25 +1,25 @@
-using NUnit.Framework;
 using UnityEngine;
 
 public class AnimHandler : MonoBehaviour
 {
     [SerializeField] private Animator anim;
     private const string walkParametr = "Walk";
-    void Start()
-    {
-
-    }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W)) 
+        if (Input.GetKeyDown(KeyCode.W))
         {
             anim.SetBool(walkParametr, true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetBool(walkParametr, false);
         }
     }
 
     public void SetWalk(bool walk)
     {
-        Animation.SetBool(walkParametr, Is);
+        anim.SetBool(walkParametr, walk);
     }
 }
